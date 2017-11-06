e537b8420eb25fc6445ce394fb62ed1d999f6496
SergeyGaponov <sergey.gaponovv@gmail.com>
2017-11-06 11:20:45 +0700

    Работа с БД

---
+++ ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/bible/BibleActivity.java
@@ -0,0 +1,15 @@
+package ru.app.churchofchrist.bible;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.app.churchofchrist.R;
+
+public class BibleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nrt_bible);
+    }
+}
