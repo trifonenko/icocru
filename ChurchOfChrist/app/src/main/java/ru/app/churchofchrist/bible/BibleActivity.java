c51d5cb4d2e533c4677d6ddb7bb1f901d687cb1e
SergeyGaponov <sergey.gaponovv@gmail.com>
2017-11-19 17:05:25 +0700

    Работа с БД

--- ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/bible/BibleActivity.java
+++ ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/bible/BibleActivity.java
@@ -6,8 +6,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 import java.io.IOException;
 import ru.app.churchofchrist.R;
@@ -19,11 +22,19 @@ public class BibleActivity extends AppCompatActivity {
 
     private DatabaseHelper mDBHelper;
     private SQLiteDatabase mDb;
+    String[] books;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nrt_bible);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
 
         mDBHelper = new DatabaseHelper(this);
 
@@ -42,6 +53,25 @@ public class BibleActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
 
+        String product = "";
+
+        Cursor cursor = mDb.rawQuery("SELECT * FROM books", null);
+        cursor.moveToFirst();
+
+        int i = 0;
+        books = new String[66];
+        while (!cursor.isAfterLast()) {
+            books[i] = cursor.getString(3);
+            i++;
+            cursor.moveToNext();
+        }
+        cursor.close();
+
+        Spinner listBooks = (Spinner) findViewById(R.id.list_books);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, books);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        listBooks.setAdapter(adapter);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,9 +79,12 @@ public class BibleActivity extends AppCompatActivity {
 
                 Cursor cursor = mDb.rawQuery("SELECT * FROM books", null);
                 cursor.moveToFirst();
+                int i = 0;
                 while (!cursor.isAfterLast()) {
                     product += cursor.getString(3) + "\n";
                     cursor.moveToNext();
+                    books[i] = product;
+                    i++;
                 }
                 cursor.close();
 
