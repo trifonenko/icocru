9f11a6e4bd234b504e5b9e268bb2006bdff3c5e5
Trifonenko <pol2naz3trif2>
2018-01-08 16:40:53 +0700

    Бэкап

--- ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/TheBackupAgent.java
+++ ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/TheBackupAgent.java
@@ -6,21 +6,23 @@ import android.app.backup.FileBackupHelper;
 
 
 public class TheBackupAgent extends BackupAgentHelper {
-    // The name of the SharedPreferences file
+    // Имя файла SharedPreferences
     static final String HIGH_SCORES_FILENAME = "scores";
 
-    // A key to uniquely identify the set of backup data
+    // Ключ к уникальной идентификации набора резервных данных
     static final String FILES_BACKUP_KEY = "myDataBase";
 
-    // Allocate a helper and add it to the backup agent
-    public void requestBackup() {
-        BackupManager bm = new BackupManager(this);
-        bm.dataChanged();
-    }
+
     @Override
     public void onCreate() {
         FileBackupHelper helper = new FileBackupHelper(this, HIGH_SCORES_FILENAME);
         addHelper(FILES_BACKUP_KEY, helper);
+    }
+
+    // Выделите помощника и добавьте его в резервный агент
+    public void requestBackup() {
+        BackupManager bm = new BackupManager(this);
+        bm.dataChanged();
     }
 
 }
