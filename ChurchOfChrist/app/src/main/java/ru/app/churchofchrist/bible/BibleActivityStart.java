dacf42bab18049edc5730a1b29b620d3661805e8
Trifonenko <pol2naz3trif2>
2018-02-13 19:50:19 +0700

    библия

--- ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/bible/BibleActivityStart.java
+++ ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/bible/BibleActivityStart.java
@@ -5,13 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.webkit.WebView;
 
 import ru.app.churchofchrist.R;
 
 public class BibleActivityStart extends AppCompatActivity {
-    private WebView webView_otr;
-    private WebView webView_search;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
