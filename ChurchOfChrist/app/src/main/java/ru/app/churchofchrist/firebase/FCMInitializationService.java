4727067d66d3931a1dc1136552b7cdd4736ee3c7
Trifonenko <trifonenko@gmail.com>
2018-02-20 16:57:06 +0700

    Firebase

---
+++ ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/firebase/FCMInitializationService.java
@@ -0,0 +1,21 @@
+package ru.app.churchofchrist.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by npanigrahy on 13/08/2016.
+ */
+public class FCMInitializationService extends FirebaseInstanceIdService {
+    private static final String TAG = "FCMInitializationService";
+
+    @Override
+    public void onTokenRefresh() {
+        String fcmToken = FirebaseInstanceId.getInstance().getToken();
+
+        Log.d(TAG, "FCM Device Token:" + fcmToken);
+        //Save or send FCM registration token
+    }
+}
