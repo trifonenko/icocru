8c8af16b1d3f671e13cd5ebb841eab245ebeae00
SergeyGaponov <sergey.gaponovv@gmail.com>
2018-02-27 12:44:48 +0700

    Good news.

--- ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/good_news/GoodNewsActivity.java
+++ ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/good_news/GoodNewsActivity.java
@@ -1,11 +1,15 @@
 package ru.app.churchofchrist.good_news;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import ru.app.churchofchrist.R;
 
@@ -14,6 +18,7 @@ import ru.app.churchofchrist.R;
  */
 
 public class GoodNewsActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +34,38 @@ public class GoodNewsActivity extends AppCompatActivity {
                 finish();
             }
         });
-        String[] str = {"1", "2", "3"};
-        RecyclerView recyclerView = findViewById(R.id.recycler_good_news);
-        GoodNewsRecyclerAdapter adapter = new GoodNewsRecyclerAdapter(str);
-        recyclerView.setAdapter(adapter);
+        recyclerView = findViewById(R.id.recycler_good_news);
         LinearLayoutManager manager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(manager);
+
+        GoodNewsTask task = new GoodNewsTask();
+        task.execute();
+    }
+
+    class GoodNewsTask extends AsyncTask<Void, Void, Void> {
+
+        String[] title = new String[1];
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Document doc = null;
+            try {
+                doc = Jsoup.connect("http://www.icocnews.ru").get();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (doc!=null)
+                title[0] = doc.title();
+            else
+                title[0] = "Ошибка";
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            GoodNewsRecyclerAdapter adapter = new GoodNewsRecyclerAdapter(title);
+            recyclerView.setAdapter(adapter);
+        }
     }
 }
