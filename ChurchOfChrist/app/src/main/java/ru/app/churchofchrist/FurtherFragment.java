01d8f32ea5a31c67c5b323166adc84b56183ada3
Sergey Gaponov <sg.gpnv@gmail.com>
2018-08-31 08:12:57 +0700

    Revert "Merge branch 'master' of D:\Project ICOC\icocru with conflicts."
    This reverts commit 8dce458

--- ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/FurtherFragment.java
+++ ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/FurtherFragment.java
@@ -9,7 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class BottomNavigationFurtherFragment extends Fragment {
+public class FurtherFragment extends Fragment {
 
     @Override
     public void onAttach(Context context) {
@@ -20,6 +20,6 @@ public class BottomNavigationFurtherFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bottom_navigation_further, container, false);
+        return inflater.inflate(R.layout.fragment_further, container, false);
     }
 }
--- ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/MainActivity.java
+++ ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/MainActivity.java
@@ -21,26 +21,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final FragmentManager manager = getSupportFragmentManager();
-        Fragment fragment = manager.findFragmentById(R.id.frame_container);
-        if (fragment == null) {
-            Fragment mainFragment = new BottomNavigationMainFragment();
-            manager.beginTransaction().add(R.id.frame_container, mainFragment).commit();
-        }
+        final FragmentManager fragmentManager = getSupportFragmentManager();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.main:
-                        Fragment mainFragment = new BottomNavigationMainFragment();
-                        manager.beginTransaction().replace(R.id.frame_container, mainFragment).commit();
-                        break;
                     case R.id.further:
-                        Fragment furtherFragment = new BottomNavigationFurtherFragment();
-                        manager.beginTransaction().replace(R.id.frame_container, furtherFragment).commit();
-                        break;
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        Fragment fragment = new FurtherFragment();
+                        fragmentTransaction.replace(R.id.frame_container, fragment);
+                        fragmentTransaction.commit();
                 }
                 return true;
             }
