c0f100e6f1b9faf38f562becff98cc8d1aecb402
Sergey Gaponov <sg.gpnv@gmail.com>
2018-08-31 08:13:26 +0700

    Revert "Menu BottomNavigationView"
    This reverts commit e82f270

--- ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/MainActivity.java
+++ ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/MainActivity.java
@@ -1,15 +1,8 @@
 package ru.app.churchofchrist;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,21 +14,5 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final FragmentManager fragmentManager = getSupportFragmentManager();
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.further:
-                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                        Fragment fragment = new FurtherFragment();
-                        fragmentTransaction.replace(R.id.frame_container, fragment);
-                        fragmentTransaction.commit();
-                }
-                return true;
-            }
-        });
     }
 }
