616a922ac053a4a2c644fb2a0a11e02a767a4437
Sergey Gaponov <sg.gpnv@gmail.com>
2018-08-31 08:11:31 +0700

    Revert "Небольшие исправления"
    This reverts commit b1524da

--- ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/bible/BibleStartFragment.java
+++ ChurchOfChrist/app/src/main/java/ru/app/churchofchrist/bible/BibleStartFragment.java
@@ -69,9 +69,6 @@ public class BibleStartFragment extends Fragment {
         dateFormat = new SimpleDateFormat("dd MMMM");
         textViewDate.setText("Чтение на сегодня (" + dateFormat.format( currentDate ) + ")");
         return view;
-
-        //Alertdialog start stih
-
     }
 
     @SuppressLint("StaticFieldLeak")
